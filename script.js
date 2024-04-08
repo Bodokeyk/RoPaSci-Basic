bc1844e2a9d94797f5eeaeb05f06baae991c04fb
bobodokey <94241624+Bodokeyk@users.noreply.github.com>
2024-04-08 16:19:18 -0600

    Add Functional javascript, also minor changes on html and css

--- script.js
+++ script.js
@@ -1,48 +1,127 @@
-const options = new Array("Rock","Paper","Scissors");
+const modeSelector = document.querySelector("#modeButton");
+const instruccionsDiv = document.querySelector(".instructions");
+const playerScore = document.querySelector(".player-score");
+const rivalScore = document.querySelector(".rival-score");
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const buttonsContainer = document.querySelector(".main-container")
+let modeNumber = true;
+let roundsCount = 0;
+let resultText;
+
+/* Listener for Mode selection, reset scores and also fixes the div */
+modeSelector.addEventListener("click", ()=> {
+let buttonContent = modeSelector.textContent;
+if (modeNumber == true){
+  modeSelector.textContent = "5 Rounds";
+  modeNumber = false;
+  playerScore.textContent = 0;
+  rivalScore.textContent = 0;
+  instruccionsDiv.textContent = "Best of 5 Mode, Good Luck!"
+  roundsCount = 0;
+}else{
+  modeSelector.textContent = "Single Game";
+  modeNumber = true
+  playerScore.textContent = 0;
+  rivalScore.textContent = 0;
+  instruccionsDiv.textContent = "Free Play, Enjoy"
+  roundsCount = 0;
+}
+});
+
+
+
+/* funcion to get the computer choice */
+const elections = new Array("Rock","Paper","Scissors");
 function getComputerChoice (){
-  let randomChoice = options[Math.floor(Math.random() * 3)] 
-console.log(randomChoice)
+  let randomChoice = elections[Math.floor(Math.random() * 3)] 
+
 return randomChoice 
 }
-
-
-
-function round(playerSelection,computerSelection) {
-  const jugador = playerSelection.toLowerCase();
-  const computadora = computerSelection.toLowerCase();
-  const jugadorNum = pasarEleccion(jugador);
-  const computadoraNum = pasarEleccion(computadora)
-  if (jugadorNum == 1 || jugadorNum == 2|| jugadorNum == 4){
-    let resultado = jugadorNum - computadoraNum;
-    
-    
-    if (resultado == 0) {return("Empate, ambos sacaron " + computadora)
-}else if (resultado == 1 || resultado == -3 || resultado == 2){
-     return(`Player, you won!! ${jugador} beats ${computadora}`)
-    }else{
-      return(`You lost!!, ${jugador} loses against ${computadora}`);
-    }
-  } else {
-    console.log("Enter a valid option player")
-  }
-}
-
-function pasarEleccion (texto) {
-  let textoPasar = texto.toLowerCase();
-  if (textoPasar =="rock"){
+/* This function converts the text of any election to numbers used on 
+the algorithm  */
+function passElection (text) {
+  let textToPass = text.toLowerCase();
+  if (textToPass =="rock"){
     return 1
   }
-  if (textoPasar =="paper"){
+  if (textToPass =="paper"){
     return 2
   }
-  if (textoPasar =="scissors"){
+  if (textToPass =="scissors"){
     return 4
   }
+}/* Event listener to Start the game(call singleGame function)
+ also pass in the choice done */
+buttonsContainer.addEventListener("click",(event) =>{
+  let buttonSelected = event.target.id;
+  if(modeNumber == true){
+  singleGame(buttonSelected, getComputerChoice())
+  }else{
+    if(roundsCount == 6){
+      playerScore.textContent = 0;
+      rivalScore.textContent = 0;
+      roundsCount = 0;
+    }
+    singleGame(buttonSelected, getComputerChoice())
+     if(rivalScore.textContent == 3 || playerScore.textContent == 3){
+        if(rivalScore.textContent > 2){
+          resultText = "Bad Luck, Your Rival Won. Click any selection to play Again";
+          instruccionsDiv.textContent = ""
+          instruccionsPrinter(resultText);
+          roundsCount = 6;
+          console.log(roundsCount)
+
+        }else{
+          resultText = "Amazing, You are the Winner!!! Click any selection to play Again";
+          instruccionsDiv.textContent = ""
+          instruccionsPrinter(resultText);
+          roundsCount = 6;
+        }
+     }
+    }
+})
+/* Function to play a single game, gets elections and compares them
+depending on it, changes its behaviour */
+function singleGame(PlayerSelec, ComputerSelec){
+  let playerNumb = passElection(PlayerSelec);
+  let computerNumb = passElection(ComputerSelec);
+  const gameResult = playerNumb - computerNumb;
+  instruccionsDiv.textContent = "";
+
+
+  if(gameResult == 0){
+    resultText = `It Was a Tie, Both selected ${PlayerSelec}. Play Again`;
+    instruccionsPrinter(resultText);
+  }else{
+
+
+    if(gameResult == 1 || gameResult == -3 || gameResult == 2){
+      resultText = `Player Won, ${PlayerSelec} beats ${ComputerSelec}!!!`;
+      playerScore.textContent = +playerScore.textContent + 1;
+      if(modeNumber == false)roundsCount = roundsCount + 1;
+      instruccionsPrinter(resultText);
+    }else{
+      resultText = `Rival Won, ${ComputerSelec} beats ${PlayerSelec}!`;
+      rivalScore.textContent = +rivalScore.textContent + 1;
+      if(modeNumber == false)roundsCount = roundsCount + 1;
+      instruccionsPrinter(resultText);
+    }
+    console.log("single Game completed")
+  }
+
 }
-function playRound(){
+function instruccionsPrinter(text){
+let paragraphToPaste = document.createElement("P");
+paragraphToPaste.textContent = text;
+instruccionsDiv.appendChild(paragraphToPaste);
+}
+
+/* function playRound(){
   for (let i = 0; i < 5;i++){
     let seleccion = prompt("rock, paper or scissors");
     let computer_seleccion = getComputerChoice();
     console.log(round(seleccion,computer_seleccion))
   }
-}
+} */
